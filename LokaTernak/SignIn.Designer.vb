2d6c8934a23cac7cdcdd81c2c6176136c511c46b
Achmad Naufal Ferdiansyah <naufansyah31@gmail.com>
2024-10-20 23:19:51 +0700

    save checkpoint

--- LokaTernak/SignIn.Designer.vb
+++ LokaTernak/SignIn.Designer.vb
@@ -45,9 +45,10 @@ Partial Class Sign_In
         Me.Panel1.BackColor = System.Drawing.Color.White
         Me.Panel1.Controls.Add(Me.text_username)
         Me.Panel1.Controls.Add(Me.PictureBox2)
-        Me.Panel1.Location = New System.Drawing.Point(79, 296)
+        Me.Panel1.Location = New System.Drawing.Point(59, 237)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(323, 58)
+        Me.Panel1.Size = New System.Drawing.Size(242, 46)
         Me.Panel1.TabIndex = 9
         '
         'text_username
@@ -55,17 +56,19 @@ Partial Class Sign_In
         Me.text_username.BorderStyle = System.Windows.Forms.BorderStyle.None
         Me.text_username.Font = New System.Drawing.Font("Segoe UI", 10.125!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.text_username.ForeColor = System.Drawing.Color.Black
-        Me.text_username.Location = New System.Drawing.Point(65, 11)
+        Me.text_username.Location = New System.Drawing.Point(49, 9)
+        Me.text_username.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.text_username.Name = "text_username"
-        Me.text_username.Size = New System.Drawing.Size(240, 36)
+        Me.text_username.Size = New System.Drawing.Size(180, 27)
         Me.text_username.TabIndex = 3
         '
         'PictureBox2
         '
         Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
-        Me.PictureBox2.Location = New System.Drawing.Point(7, 9)
+        Me.PictureBox2.Location = New System.Drawing.Point(5, 7)
+        Me.PictureBox2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.PictureBox2.Name = "PictureBox2"
-        Me.PictureBox2.Size = New System.Drawing.Size(40, 40)
+        Me.PictureBox2.Size = New System.Drawing.Size(30, 32)
         Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox2.TabIndex = 3
         Me.PictureBox2.TabStop = False
@@ -75,18 +78,20 @@ Partial Class Sign_In
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Segoe UI Black", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.Color.Black
-        Me.Label1.Location = New System.Drawing.Point(178, 211)
+        Me.Label1.Location = New System.Drawing.Point(134, 169)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(129, 45)
+        Me.Label1.Size = New System.Drawing.Size(95, 32)
         Me.Label1.TabIndex = 8
         Me.Label1.Text = "Sign In"
         '
         'PictureBox1
         '
         Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
-        Me.PictureBox1.Location = New System.Drawing.Point(166, 35)
+        Me.PictureBox1.Location = New System.Drawing.Point(124, 28)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(156, 156)
+        Me.PictureBox1.Size = New System.Drawing.Size(117, 125)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox1.TabIndex = 7
         Me.PictureBox1.TabStop = False
@@ -96,9 +101,10 @@ Partial Class Sign_In
         Me.Panel2.BackColor = System.Drawing.Color.White
         Me.Panel2.Controls.Add(Me.text_password)
         Me.Panel2.Controls.Add(Me.PictureBox3)
-        Me.Panel2.Location = New System.Drawing.Point(79, 370)
+        Me.Panel2.Location = New System.Drawing.Point(59, 296)
+        Me.Panel2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Panel2.Name = "Panel2"
-        Me.Panel2.Size = New System.Drawing.Size(323, 58)
+        Me.Panel2.Size = New System.Drawing.Size(242, 46)
         Me.Panel2.TabIndex = 10
         '
         'text_password
@@ -106,17 +112,19 @@ Partial Class Sign_In
         Me.text_password.BorderStyle = System.Windows.Forms.BorderStyle.None
         Me.text_password.Font = New System.Drawing.Font("Segoe UI", 10.125!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.text_password.ForeColor = System.Drawing.Color.Black
-        Me.text_password.Location = New System.Drawing.Point(65, 11)
+        Me.text_password.Location = New System.Drawing.Point(49, 9)
+        Me.text_password.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.text_password.Name = "text_password"
-        Me.text_password.Size = New System.Drawing.Size(240, 36)
+        Me.text_password.Size = New System.Drawing.Size(180, 27)
         Me.text_password.TabIndex = 3
         '
         'PictureBox3
         '
         Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
-        Me.PictureBox3.Location = New System.Drawing.Point(7, 9)
+        Me.PictureBox3.Location = New System.Drawing.Point(5, 7)
+        Me.PictureBox3.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.PictureBox3.Name = "PictureBox3"
-        Me.PictureBox3.Size = New System.Drawing.Size(40, 40)
+        Me.PictureBox3.Size = New System.Drawing.Size(30, 32)
         Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox3.TabIndex = 3
         Me.PictureBox3.TabStop = False
@@ -126,9 +134,10 @@ Partial Class Sign_In
         Me.button_cancel.FlatAppearance.BorderSize = 0
         Me.button_cancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.button_cancel.Font = New System.Drawing.Font("Segoe UI", 7.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.button_cancel.Location = New System.Drawing.Point(79, 532)
+        Me.button_cancel.Location = New System.Drawing.Point(59, 426)
+        Me.button_cancel.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.button_cancel.Name = "button_cancel"
-        Me.button_cancel.Size = New System.Drawing.Size(323, 57)
+        Me.button_cancel.Size = New System.Drawing.Size(242, 46)
         Me.button_cancel.TabIndex = 12
         Me.button_cancel.Text = "Cancel"
         Me.button_cancel.UseVisualStyleBackColor = True
@@ -140,25 +149,25 @@ Partial Class Sign_In
         Me.button_login.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.button_login.Font = New System.Drawing.Font("Segoe UI", 7.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.button_login.ForeColor = System.Drawing.Color.White
-        Me.button_login.Location = New System.Drawing.Point(79, 469)
+        Me.button_login.Location = New System.Drawing.Point(59, 375)
+        Me.button_login.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.button_login.Name = "button_login"
-        Me.button_login.Size = New System.Drawing.Size(323, 57)
+        Me.button_login.Size = New System.Drawing.Size(242, 46)
         Me.button_login.TabIndex = 11
         Me.button_login.Text = "Login"
         Me.button_login.UseVisualStyleBackColor = False
         '
         'Sign_In
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(481, 624)
+        Me.ClientSize = New System.Drawing.Size(361, 499)
         Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.Panel2)
         Me.Controls.Add(Me.button_cancel)
         Me.Controls.Add(Me.button_login)
-        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "Sign_In"
