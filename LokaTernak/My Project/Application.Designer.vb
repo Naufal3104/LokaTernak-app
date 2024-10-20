0e2f59171b1d2d054681d1614a8c6bdbc581a16b
ahnafzaki12 <ahnafzaki.b@gmail.com>
2024-10-20 22:52:07 +0700

    UBAH BARU

--- LokaTernak/My Project/Application.Designer.vb
+++ LokaTernak/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.LokaTernak.Dashboard_Admin
+            Me.MainForm = Global.LokaTernak.Dashboard_Admin2
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
