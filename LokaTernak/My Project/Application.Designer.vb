1fcf3ec09963fa52deff841ad3ecee2d378f2bcb
23082010146@student.upnjatim.ac.id <23082010146@student.upnjatim.ac.id>
2024-10-21 11:07:05 +0700

    dashboard user

--- LokaTernak/My Project/Application.Designer.vb
+++ LokaTernak/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.LokaTernak.Dashboard_User
+            Me.MainForm = Global.LokaTernak.Dashboard_Admin_3
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
